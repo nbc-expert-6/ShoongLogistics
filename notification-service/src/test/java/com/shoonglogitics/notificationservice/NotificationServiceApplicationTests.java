874d23558e5f9b39a7e19dc93de4b034f55fd991
skfkgla <narahim.lee@gmail.com>
2025-11-03 15:49:50 +0900

    init commit

---
+++ notification-service/src/test/java/com/shoonglogitics/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoonglogitics.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/com/shoonglogitics/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoonglogitics.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
