2a3129ca1191b666b8abbeef6a541eb2032eb063
kangkigs <dlflq11@gmail.com>
2025-11-04 20:13:19 +0900

    [feat] 배송 도메인 4계층 구조 구현

--- order-service/src/main/java/com/shoonglogitics/orderservice/delivery/application/service/DeliveryService.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/delivery/application/service/DeliveryService.java
@@ -1,7 +1,13 @@
 package com.shoonglogitics.orderservice.delivery.application.service;
 
+import com.shoonglogitics.orderservice.delivery.domain.repository.DeliveryRepository;
+import com.shoonglogitics.orderservice.delivery.domain.service.DeliveryDomainService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DeliveryService {
+    private final DeliveryRepository deliveryRepository;
+    private final DeliveryDomainService domainService;
 }
--- order-service/src/main/java/com/shoonglogitics/orderservice/delivery/infrastructure/config/DomainServiceConfig.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/delivery/infrastructure/config/DomainServiceConfig.java
@@ -1,4 +1,14 @@
 package com.shoonglogitics.orderservice.delivery.infrastructure.config;
 
+import com.shoonglogitics.orderservice.delivery.domain.service.DeliveryDomainService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DomainServiceConfig {
+
+    @Bean
+    public DeliveryDomainService domainService() {
+        return new DeliveryDomainService();
+    }
 }
