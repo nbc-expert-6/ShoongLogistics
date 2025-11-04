383e182dd6dcb0f4c449bd980ed787ed86435569
kangkigs <dlflq11@gmail.com>
2025-11-04 15:44:55 +0900

    [refactor] BaseEntity,OrderEntity 수정

--- order-service/src/main/java/com/shoonglogitics/orderservice/common/entity/BaseEntity.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/common/entity/BaseEntity.java
@@ -1,17 +1,54 @@
 package com.shoonglogitics.orderservice.common.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-public interface BaseEntity {
-    LocalDateTime getCreatedAt();
-    Long getCreatedBy();
-    LocalDateTime getUpdatedAt();
-    Long getUpdatedBy();
-    LocalDateTime getDeletedAt();
-    Long getDeletedBy();
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity implements BaseAuditEntity {
 
-    void update(Long userId);
-    void softDelete(Long userId);
-    boolean isDeleted();
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    private Long deletedBy;
+
+    @Override
+    public void update(Long userId) {
+        this.updatedAt = LocalDateTime.now();
+        this.updatedBy = userId;
+    }
+
+    @Override
+    public void softDelete(Long userId) {
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = userId;
+    }
+
+    @Override
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
 }
