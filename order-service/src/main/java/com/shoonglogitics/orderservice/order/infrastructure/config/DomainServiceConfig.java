2041d515a1410d348ce3d9d74992a55da1f8f392
kangkigs <dlflq11@gmail.com>
2025-11-04 15:25:54 +0900

    [feat] 주문 도메인 4계층 구조 구현

---
+++ order-service/src/main/java/com/shoonglogitics/orderservice/order/infrastructure/config/DomainServiceConfig.java
@@ -0,0 +1,15 @@
+package com.shoonglogitics.orderservice.order.infrastructure.config;
+
+import com.shoonglogitics.orderservice.order.domain.repository.OrderRepository;
+import com.shoonglogitics.orderservice.order.domain.service.OrderDomainService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DomainServiceConfig {
+
+    @Bean
+    public OrderDomainService orderDomainService() {
+        return new OrderDomainService();
+    }
+}
--- order-service/src/main/java/com/shoonglogitics/orderservice/order/infrastructure/config/RepositoryConfig.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/order/infrastructure/config/RepositoryConfig.java
@@ -1,4 +1,16 @@
 package com.shoonglogitics.orderservice.order.infrastructure.config;
 
+import com.shoonglogitics.orderservice.order.domain.repository.OrderRepository;
+import com.shoonglogitics.orderservice.order.infrastructure.repository.JpaOrderRepository;
+import com.shoonglogitics.orderservice.order.infrastructure.repository.OrderRepositoryAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RepositoryConfig {
+
+    @Bean
+    public OrderRepository orderRepository(JpaOrderRepository jpaOrderRepository) {
+        return new OrderRepositoryAdapter(jpaOrderRepository);
+    }
 }
