5c196d652b48486907bc13a332548e87e679ae96
kangkigs <dlflq11@gmail.com>
2025-11-05 15:26:34 +0900

    [feat] 요청에 사용할 dto 구현

--- order-service/src/main/java/com/shoonglogitics/orderservice/domain/order/application/OrderService.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/domain/order/application/OrderService.java
@@ -1,14 +1,24 @@
 package com.shoonglogitics.orderservice.domain.order.application;
 
-import com.shoonglogitics.orderservice.domain.order.domain.repository.OrderRepository;
-import com.shoonglogitics.orderservice.domain.order.domain.service.OrderDomainService;
-import lombok.RequiredArgsConstructor;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.shoonglogitics.orderservice.domain.order.application.command.CreateOrderCommand;
+import com.shoonglogitics.orderservice.domain.order.domain.repository.OrderRepository;
+import com.shoonglogitics.orderservice.domain.order.domain.service.OrderDomainService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j(topic = "order-service")
 @RequiredArgsConstructor
 public class OrderService {
-    private final OrderRepository orderRepository;
-    private final OrderDomainService orderDomainService;
+	private final OrderRepository orderRepository;
+	private final OrderDomainService orderDomainService;
 
+	public UUID createOrder(CreateOrderCommand command) {
+		log.info("Create order: {}", command);
+	}
 }
--- order-service/src/main/java/com/shoonglogitics/orderservice/domain/order/presentation/OrderController.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/domain/order/presentation/OrderController.java
@@ -1,14 +1,44 @@
 package com.shoonglogitics.orderservice.domain.order.presentation;
 
-import com.shoonglogitics.orderservice.domain.order.application.OrderService;
-import lombok.RequiredArgsConstructor;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shoonglogitics.orderservice.domain.order.application.OrderService;
+import com.shoonglogitics.orderservice.domain.order.application.command.CreateOrderCommand;
+import com.shoonglogitics.orderservice.domain.order.presentation.dto.CreateOrderRequest;
+import com.shoonglogitics.orderservice.domain.order.presentation.dto.CreateOrderResponse;
+import com.shoonglogitics.orderservice.global.common.exception.ApiResponse;
+import com.shoonglogitics.orderservice.global.common.vo.UserRoleType;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@Slf4j
+@RequestMapping("/api/v1/orders")
 public class OrderController {
 
-    private final OrderService orderService;
+	private final OrderService orderService;
+
+	@PostMapping
+	public ResponseEntity<ApiResponse> createOrder(
+		@Valid @RequestBody CreateOrderRequest request,
+		@RequestHeader("X-User-Id") Long userId,
+		@RequestHeader("X-User-Role") UserRoleType role) {
+		UUID orderId = orderService.createOrder(CreateOrderCommand.from(request, userId, role));
+		CreateOrderResponse response = new CreateOrderResponse(
+			orderId,
+			"주문이 생성에 성공했습니다."
+		);
+		return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(response));
+	}
 }
