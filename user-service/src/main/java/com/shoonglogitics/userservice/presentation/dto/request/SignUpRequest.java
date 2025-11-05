f8dfa5e95a570b92cd7545b8a3c077959c4214d9
brian110326 <brian110326@gmail.com>
2025-11-05 09:02:37 +0900

    [refactor] : 회원가입 JsonTypeInfo로 리팩터링

--- user-service/src/main/java/com/shoonglogitics/userservice/presentation/dto/request/SignUpRequest.java
+++ user-service/src/main/java/com/shoonglogitics/userservice/presentation/dto/request/SignUpRequest.java
@@ -1,32 +1,42 @@
 package com.shoonglogitics.userservice.presentation.dto.request;
 
-import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.shoonglogitics.userservice.application.command.CompanyManagerSignUpCommand;
+import com.shoonglogitics.userservice.application.command.ShipperSignUpCommand;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class SignUpRequest {
+@JsonTypeInfo(
+	use = JsonTypeInfo.Id.NAME,
+	include = JsonTypeInfo.As.PROPERTY,
+	property = "userType"
+)
+@JsonSubTypes({
+	@JsonSubTypes.Type(value = MasterSignUpRequest.class, name = "MASTER"),
+	@JsonSubTypes.Type(value = HubManagerSignUpRequest.class, name = "HUB_MANAGER"),
+	@JsonSubTypes.Type(value = CompanyManagerSignUpCommand.class, name = "COMPANY_MANAGER"),
+	@JsonSubTypes.Type(value = ShipperSignUpCommand.class, name = "SHIPPER")
+})
+public abstract class SignUpRequest {
 
 	private String userName;
 	private String password;
-	private String userType; // MASTER, HUB_MANAGER, COMPANY_MANAGER, SHIPPER
+	//private String userType; // MASTER, HUB_MANAGER, COMPANY_MANAGER, SHIPPER
 
 	private String email;
 	private String name;
 	private String slackId;
 	private String phoneNumber;
 
-	private UUID hubId;       // HUB_MANAGER, COMPANY_SHIPPER
+	/*private UUID hubId;       // HUB_MANAGER, COMPANY_SHIPPER
 	private UUID companyId;   // COMPANY_MANAGER
 
 	private String shipperType; // SHIPPER
 	private Integer order;      // SHIPPER
-	private Boolean isShippingAvailable; // SHIPPER
+	private Boolean isShippingAvailable; // SHIPPER*/
+
+	public abstract <T> T toCommand();
 
 }
