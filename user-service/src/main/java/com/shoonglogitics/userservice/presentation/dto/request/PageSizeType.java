4ed6a5d0a4d7ecf9ca44a1a1e49ad83a79bdec1a
brian110326 <brian110326@gmail.com>
2025-11-06 20:29:27 +0900

    [feat] : 회원 목록 조회 시 정렬, 고정 PageSizeType 적용

---
+++ user-service/src/main/java/com/shoonglogitics/userservice/presentation/dto/request/PageSizeType.java
@@ -0,0 +1,16 @@
+package com.shoonglogitics.userservice.presentation.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public enum PageSizeType {
+	SIZE_10(10),
+	SIZE_30(30),
+	SIZE_50(50);
+
+	private final int value;
+
+	PageSizeType(int value) {
+		this.value = value;
+	}
+}
