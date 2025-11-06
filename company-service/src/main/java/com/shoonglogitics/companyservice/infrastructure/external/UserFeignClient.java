4af78c3902804268f375248554920a648b9fd468
skfkgla <narahim.lee@gmail.com>
2025-11-06 14:42:20 +0900

    [refactor] 고객 생성시 외부 API에 인증 헤더를 담도록 구현

--- company-service/src/main/java/com/shoonglogitics/companyservice/infrastructure/external/UserFeignClient.java
+++ company-service/src/main/java/com/shoonglogitics/companyservice/infrastructure/external/UserFeignClient.java
@@ -1,11 +1,18 @@
 package com.shoonglogitics.companyservice.infrastructure.external;
 
+import java.util.UUID;
+
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shoonglogitics.companyservice.application.service.UserClient;
+import com.shoonglogitics.companyservice.infrastructure.security.HeaderType;
 import com.shoonglogitics.companyservice.presentation.company.common.dto.ApiResponse;
+import com.shoonglogitics.companyservice.presentation.company.common.dto.PageResponse;
 
 @FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserFeignClient {
@@ -13,5 +20,16 @@ public interface UserFeignClient {
 	 * User Service에서 사용자 정보 조회
 	 */
 	@GetMapping("/api/v1/users/{userId}")
-	ApiResponse<UserClient.UserInfo> getUserInfo(@PathVariable("userId") Long userId);
+	ApiResponse<UserClient.UserInfo> getUserInfo(@PathVariable("userId") Long userId,
+		@RequestHeader(HeaderType.USER_ID) Long currentUserId,
+		@RequestHeader(HeaderType.USER_ROLE) String currentUserRole);
+
+	@GetMapping("/api/v1/users")
+	ApiResponse<PageResponse<UserClient.UserInfo>> getUserInfos(@RequestParam("companyId") UUID companyId,
+		@RequestParam("isDeleted") Boolean isDeleted,
+		@RequestHeader(HeaderType.USER_ID) Long currentUserId,
+		@RequestHeader(HeaderType.USER_ROLE) String currentUserRole);
+
+	@DeleteMapping("/api/v1/users/{userId}")
+	ApiResponse<String> deleteUser(@PathVariable("userId") Long userId);
 }
