0f8737d2eb8ab6343e64ee2933839f78f56841a3
brian110326 <brian110326@gmail.com>
2025-11-06 19:29:35 +0900

    [fix] : application.yml 수정, postgres 추가

--- user-service/src/main/java/com/shoonglogitics/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/shoonglogitics/userservice/UserServiceApplication.java
@@ -3,12 +3,13 @@ package com.shoonglogitics.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ComponentScan("com.shoonglogitics")
 @EnableJpaAuditing
-//@Profile(value = "test")
+@Profile(value = "test")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
