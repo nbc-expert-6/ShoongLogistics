d7818d632483a5df81642e8f15b4e8f7e97a4a34
skfkgla <narahim.lee@gmail.com>
2025-11-06 19:33:40 +0900

    feat: 업체 목록 조회 API 리베이스 오류 해결 커밋
    # Conflicts:
    #	company-service/src/main/java/com/shoonglogitics/companyservice/application/CompanyService.java
    #	company-service/src/main/java/com/shoonglogitics/companyservice/domain/company/repository/CompanyRepository.java
    #	company-service/src/main/java/com/shoonglogitics/companyservice/infrastructure/repository/CompanyRepositoryAdapter.java
    #	company-service/src/main/java/com/shoonglogitics/companyservice/infrastructure/repository/JpaCompanyRepository.java
    #	company-service/src/main/java/com/shoonglogitics/companyservice/presentation/company/CompanyController.java

--- company-service/src/main/java/com/shoonglogitics/companyservice/application/CompanyService.java
+++ company-service/src/main/java/com/shoonglogitics/companyservice/application/CompanyService.java
@@ -3,11 +3,13 @@ package com.shoonglogitics.companyservice.application;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.shoonglogitics.companyservice.application.command.CreateCompanyCommand;
 import com.shoonglogitics.companyservice.application.command.DeleteCompanyCommand;
+import com.shoonglogitics.companyservice.application.command.GetCompaniesCommand;
 import com.shoonglogitics.companyservice.application.dto.CompanyResult;
 import com.shoonglogitics.companyservice.application.service.UserClient;
 import com.shoonglogitics.companyservice.domain.common.vo.AuthUser;
@@ -65,5 +67,10 @@ public class CompanyService {
 			.orElseThrow(() -> new IllegalArgumentException("업체를 찾을 수 없습니다."));
 
 		return CompanyResult.from(company);
+	}
+
+	public Page<CompanyResult> getCompanies(GetCompaniesCommand command) {
+		Page<Company> companies = companyRepository.getCompanies(command.hubId(), command.name(), command.type(), command.pageRequest().toPageable());
+		return companies.map(CompanyResult::from);
 	}
 }
