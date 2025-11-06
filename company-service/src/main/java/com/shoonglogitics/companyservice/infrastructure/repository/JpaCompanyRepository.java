0c43e9502aeaf29e1ddc072120604cffac5fd1e9
skfkgla <narahim.lee@gmail.com>
2025-11-06 23:14:55 +0900

    [feat] 업체 상세 조회 리베이스 오류 해결 커밋

--- company-service/src/main/java/com/shoonglogitics/companyservice/infrastructure/repository/JpaCompanyRepository.java
+++ company-service/src/main/java/com/shoonglogitics/companyservice/infrastructure/repository/JpaCompanyRepository.java
@@ -1,11 +1,13 @@
 package com.shoonglogitics.companyservice.infrastructure.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shoonglogitics.companyservice.domain.company.entity.Company;
+import com.shoonglogitics.companyservice.domain.company.vo.CompanyType;
 
 @Repository
 public interface JpaCompanyRepository extends JpaRepository<Company, UUID> {
