2a8a22f7f358c5893a0465fd529c2fb6393d3236
brian110326 <brian110326@gmail.com>
2025-11-06 19:25:49 +0900

    [fix] : 배송담당자 회원가입 시 배송순번 마지막번호 자동 부여

--- user-service/src/main/java/com/shoonglogitics/userservice/application/command/ShipperSignUpCommand.java
+++ user-service/src/main/java/com/shoonglogitics/userservice/application/command/ShipperSignUpCommand.java
@@ -17,18 +17,16 @@ public class ShipperSignUpCommand extends SignUpUserCommand {
 
 	private ShipperType shipperType;
 	private HubId hubId;
-	private Integer order;
 	private Boolean isShippingAvailable;
 
 	@Builder
 	public ShipperSignUpCommand(String userName, String password, Name name,
 		Email email, SlackId slackId, PhoneNumber phoneNumber,
 		ShipperType shipperType, HubId hubId,
-		Integer order, Boolean isShippingAvailable) { // 필드 추가
+		Boolean isShippingAvailable) { // 필드 추가
 		super(userName, password, SignupStatus.PENDING, name, email, slackId, phoneNumber);
 		this.shipperType = shipperType;
 		this.hubId = hubId;
-		this.order = order;
 		this.isShippingAvailable = isShippingAvailable;
 	}
 
