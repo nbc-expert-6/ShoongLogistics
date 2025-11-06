354eb700be9041dffa2a1c6ffd9aa4a7068d1f40
kangkigs <dlflq11@gmail.com>
2025-11-06 13:27:17 +0900

    [refactor] dto 금액 및 수량 관련 필드명,타입 수정

--- order-service/src/main/java/com/shoonglogitics/orderservice/domain/order/application/command/CreateOrderItemCommand.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/domain/order/application/command/CreateOrderItemCommand.java
@@ -1,10 +1,11 @@
 package com.shoonglogitics.orderservice.domain.order.application.command;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public record CreateOrderItemCommand(
 	UUID productId,
-	Integer price,
-	Integer amount
+	BigDecimal price,
+	Integer quantity
 ) {
 }
