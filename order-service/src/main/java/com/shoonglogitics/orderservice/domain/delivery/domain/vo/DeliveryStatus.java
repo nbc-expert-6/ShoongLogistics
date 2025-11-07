c13323e41ae339078fb5491eca672d78a2288377
kangkigs <dlflq11@gmail.com>
2025-11-07 10:18:15 +0900

    [refactor] 배송 상태 값 수정 및 함수 추가

--- order-service/src/main/java/com/shoonglogitics/orderservice/domain/delivery/domain/vo/DeliveryStatus.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/domain/delivery/domain/vo/DeliveryStatus.java
@@ -4,17 +4,69 @@ import lombok.Getter;
 
 @Getter
 public enum DeliveryStatus {
-    HUB_WAITING("허브 대기중"),
-    HUB_TRANSIT("허브 이동중"),
-    HUB_ARRIVED("목적지 허브 도착"),
-    IN_DELIVERY("배송중"),
-    TO_VENDOR("업체이동중"),
-    DELIVERED("배송완료");
+	HUB_WAITING("허브 대기중") {
+		@Override
+		public boolean canTransitionTo(DeliveryStatus newStatus) {
+			return newStatus == HUB_TRANSIT;
+		}
 
-    private final String description;
+		@Override
+		public boolean canBeCancelled() {
+			return true;
+		}
+	},
+	HUB_TRANSIT("허브 이동중") {
+		@Override
+		public boolean canTransitionTo(DeliveryStatus newStatus) {
+			return newStatus == HUB_ARRIVED;
+		}
 
-    DeliveryStatus(String description) {
-        this.description = description;
-    }
+		@Override
+		public boolean canBeCancelled() {
+			return false;
+		}
+	},
+	HUB_ARRIVED("목적지 허브 도착") {
+		@Override
+		public boolean canTransitionTo(DeliveryStatus newStatus) {
+			return newStatus == IN_DELIVERY;
+		}
 
+		@Override
+		public boolean canBeCancelled() {
+			return false;
+		}
+	},
+	IN_DELIVERY("배송중") {
+		@Override
+		public boolean canTransitionTo(DeliveryStatus newStatus) {
+			return newStatus == DELIVERED;
+		}
+
+		@Override
+		public boolean canBeCancelled() {
+			return false;
+		}
+	},
+	DELIVERED("배송완료") {
+		@Override
+		public boolean canTransitionTo(DeliveryStatus newStatus) {
+			return false; // 완료 상태에서 전이 불가
+		}
+
+		@Override
+		public boolean canBeCancelled() {
+			return false;
+		}
+	};
+
+	private final String description;
+
+	DeliveryStatus(String description) {
+		this.description = description;
+	}
+
+	public abstract boolean canTransitionTo(DeliveryStatus newStatus);
+
+	public abstract boolean canBeCancelled();
 }
