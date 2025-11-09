1323d6f5781690049cd458c0573a01a29452a7ab
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-11-09 16:19:35 +0900

    refactor : Repository 어노테이션 추가

--- hub-service/src/main/java/com/shoonglogitics/hubservice/infrastructure/repository/JpaHubRepository.java
+++ hub-service/src/main/java/com/shoonglogitics/hubservice/infrastructure/repository/JpaHubRepository.java
@@ -3,9 +3,11 @@ package com.shoonglogitics.hubservice.infrastructure.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shoonglogitics.hubservice.domain.entity.Hub;
 
+@Repository
 public interface JpaHubRepository extends JpaRepository<Hub, UUID> {
 
 }
