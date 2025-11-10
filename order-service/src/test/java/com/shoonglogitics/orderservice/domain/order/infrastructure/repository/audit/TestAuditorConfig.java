17533af14571019175ca7b7ab88dedebe01a7cc7
kangkigs <dlflq11@gmail.com>
2025-11-10 17:36:12 +0900

    [fix] bean 중복 등록 문제 해결

--- order-service/src/test/java/com/shoonglogitics/orderservice/domain/order/infrastructure/repository/audit/TestAuditorConfig.java
+++ order-service/src/test/java/com/shoonglogitics/orderservice/domain/order/infrastructure/repository/audit/TestAuditorConfig.java
@@ -2,14 +2,16 @@ package com.shoonglogitics.orderservice.domain.order.infrastructure.repository.a
 
 import java.util.Optional;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.AuditorAware;
 
-@Configuration
+@TestConfiguration
 public class TestAuditorConfig {
 
 	@Bean
+	@Primary
 	public AuditorAware<Long> auditorAwareImpl() {
 		// 테스트용으로 고정값 반환
 		return () -> Optional.of(1L);
