55c4c1010c3f1706b9c9fbea5943ff7a460b5560
kangkigs <dlflq11@gmail.com>
2025-11-10 17:26:32 +0900

    [test] 배송 도메인 인프라 테스트 코드 작성

--- order-service/src/test/java/com/shoonglogitics/orderservice/domain/delivery/domain/vo/GeoLocationTest.java
+++ order-service/src/test/java/com/shoonglogitics/orderservice/domain/delivery/domain/vo/GeoLocationTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import com.shoonglogitics.orderservice.domain.order.domain.vo.GeoLocation;
+
 class GeoLocationTest {
 
 	@Test
