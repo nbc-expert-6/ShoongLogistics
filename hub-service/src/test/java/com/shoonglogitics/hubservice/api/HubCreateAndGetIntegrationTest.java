efbd1e4ca97aaabe0bf8317075ccafcfa72facbf
Daae-Kim <wlsemrdl1212@naver.com>
2025-11-10 17:03:12 +0900

    test : 통합테스트 에러 해결

--- hub-service/src/test/java/com/shoonglogitics/hubservice/api/HubCreateAndGetIntegrationTest.java
+++ hub-service/src/test/java/com/shoonglogitics/hubservice/api/HubCreateAndGetIntegrationTest.java
@@ -11,12 +11,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+//@Disabled
+@Transactional
 @ActiveProfiles("test")
 @SpringBootTest(properties = {
         "spring.cache.type=simple"})
