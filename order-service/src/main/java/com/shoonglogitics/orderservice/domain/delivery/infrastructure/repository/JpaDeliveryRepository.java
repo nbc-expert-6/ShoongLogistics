790a05e98528989c91aa188d527119b7629544fb
kangkigs <dlflq11@gmail.com>
2025-11-10 10:47:03 +0900

    [fix] 삭제된 배송경로까지 조회되는 문제 수정

--- order-service/src/main/java/com/shoonglogitics/orderservice/domain/delivery/infrastructure/repository/JpaDeliveryRepository.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/domain/delivery/infrastructure/repository/JpaDeliveryRepository.java
@@ -15,8 +15,8 @@ public interface JpaDeliveryRepository extends JpaRepository<Delivery, UUID> {
 	Optional<Delivery> findByOrderId(UUID orderId);
 
 	@Query(
-		value = "SELECT * FROM p_delivery_route WHERE delivery_id = :deliveryId",
-		countQuery = "SELECT COUNT(*) FROM delivery_route WHERE delivery_id = :deliveryId",
+		value = "SELECT * FROM p_delivery_route WHERE delivery_id = :deliveryId AND deleted_at IS NULL",
+		countQuery = "SELECT COUNT(*) FROM delivery_route WHERE delivery_id = :deliveryId AND deleted_at IS NULL",
 		nativeQuery = true
 	)
 	Page<DeliveryRoute> findAllDeliveryRouteById(UUID deliveryId, Pageable pageable);
