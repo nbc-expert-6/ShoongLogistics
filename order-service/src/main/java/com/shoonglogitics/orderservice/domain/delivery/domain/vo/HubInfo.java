f7368a4b0b6f592330d63a8feb5c5cb6f99a1b81
kangkigs <dlflq11@gmail.com>
2025-11-10 17:07:39 +0900

    [test] 배송 도메인 vo 테스트 코드 작성

--- order-service/src/main/java/com/shoonglogitics/orderservice/domain/delivery/domain/vo/HubInfo.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/domain/delivery/domain/vo/HubInfo.java
@@ -1,22 +1,22 @@
 package com.shoonglogitics.orderservice.domain.delivery.domain.vo;
 
+import java.util.UUID;
+
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class HubInfo {
 
-    private UUID hubId;
+	private UUID hubId;
 
-    public static HubInfo of(UUID hubId) {
-        HubInfo hub = new HubInfo();
-        hub.hubId = hubId;
-        return hub;
-    }
+	public static HubInfo of(UUID hubId) {
+		HubInfo hub = new HubInfo();
+		hub.hubId = hubId;
+		return hub;
+	}
 }
