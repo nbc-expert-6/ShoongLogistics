700f4537efa23e700bfa54380ad6f565e48bd00a
Daae-Kim <wlsemrdl1212@naver.com>
2025-11-12 02:09:36 +0900

    refactor : dto record -> class 전환
    Redis 사용시 직렬화 문제로 record 에서 class 로 전환하였습니다.

--- hub-service/src/main/java/com/shoonglogitics/hubservice/presentation/dto/response/HubDetailResponse.java
+++ hub-service/src/main/java/com/shoonglogitics/hubservice/presentation/dto/response/HubDetailResponse.java
@@ -1,26 +1,38 @@
 package com.shoonglogitics.hubservice.presentation.dto.response;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 import com.shoonglogitics.hubservice.application.dto.HubResult;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record HubDetailResponse(
-	UUID hubId,
-	String name,
-	String address,
-	Double latitude,
-	Double longitude,
-	String hubType
-) {
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HubDetailResponse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private UUID hubId;
+	private String name;
+	private String address;
+	private Double latitude;
+	private Double longitude;
+	private String hubType;
 
 	public static HubDetailResponse from(HubResult hub) {
-		return new HubDetailResponse(
-			hub.hubId(),
-			hub.name(),
-			hub.address(),
-			hub.latitude(),
-			hub.longitude(),
-			hub.hubType()
-		);
+		return HubDetailResponse.builder()
+				.hubId(hub.getHubId())
+				.name(hub.getName())
+				.address(hub.getAddress())
+				.latitude(hub.getLatitude())
+				.longitude(hub.getLongitude())
+				.hubType(hub.getHubType())
+				.build();
 	}
 }
