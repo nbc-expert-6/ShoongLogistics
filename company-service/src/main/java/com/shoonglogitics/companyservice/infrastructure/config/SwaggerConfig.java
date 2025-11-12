8e998e2be418e92963ced9e16fc9ca15433ea335
skfkgla <narahim.lee@gmail.com>
2025-11-12 15:32:53 +0900

    chore: swagger header 오류 수정

--- company-service/src/main/java/com/shoonglogitics/companyservice/infrastructure/config/SwaggerConfig.java
+++ company-service/src/main/java/com/shoonglogitics/companyservice/infrastructure/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.shoonglogitics.userservice.infrastructure.config;
+package com.shoonglogitics.companyservice.infrastructure.config;
 
 import java.util.List;
 
@@ -14,32 +14,25 @@ import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
+
 	@Bean
 	public OpenAPI openAPI() {
-		String jwtSchemeName = "BearerAuth";
-
-		SecurityScheme securityScheme = new SecurityScheme()
-			.name(jwtSchemeName)
-			.type(SecurityScheme.Type.HTTP)
-			.scheme("bearer")
-			.bearerFormat("JWT")
-			.description("JWT 토큰을 입력하세요 (Bearer 제외)");
-
-		SecurityRequirement securityRequirement = new SecurityRequirement()
-			.addList(jwtSchemeName);
-
 		return new OpenAPI()
 			.info(new Info()
-				.title("User Service API")
-				.description("사용자 관리 서비스")
-				.version("1.0"))
+				.title("Company Service API")
+				.version("1.0")
+				.description("업체 관리 서비스"))
+			// ✅ Gateway를 통해 호출하도록 설정
 			.servers(List.of(
 				new Server()
 					.url("http://localhost:8000")
 					.description("API Gateway")
 			))
-			.addSecurityItem(securityRequirement)
+			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
 			.components(new Components()
-				.addSecuritySchemes(jwtSchemeName, securityScheme));
+				.addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+					.type(SecurityScheme.Type.HTTP)
+					.scheme("bearer")
+					.bearerFormat("JWT")));
 	}
 }
