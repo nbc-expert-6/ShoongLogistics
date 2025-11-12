2986b2bb94aae7e1eb47d1a861b0c193de24e36c
kangkigs <dlflq11@gmail.com>
2025-11-12 17:01:53 +0900

    [fix] 통신 오류 해결

--- order-service/src/main/java/com/shoonglogitics/orderservice/domain/delivery/infrastructure/external/client/feign/HubFeignClient.java
+++ order-service/src/main/java/com/shoonglogitics/orderservice/domain/delivery/infrastructure/external/client/feign/HubFeignClient.java
@@ -4,12 +4,11 @@ import java.util.UUID;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shoonglogitics.orderservice.domain.delivery.infrastructure.external.dto.FeignRouteResponse;
-import com.shoonglogitics.orderservice.global.common.exception.ApiResponse;
 import com.shoonglogitics.orderservice.global.common.vo.UserRoleType;
 
 @FeignClient(
@@ -17,8 +16,8 @@ import com.shoonglogitics.orderservice.global.common.vo.UserRoleType;
 	url = "${hub-service.url}"
 )
 public interface HubFeignClient {
-	@PostMapping("/api/v1/hub-routes/optimal-route")
-	ResponseEntity<ApiResponse<FeignRouteResponse>> getShippingRoutes(
+	@GetMapping("/api/v1/hub-routes/optimal-route")
+	ResponseEntity<FeignRouteResponse> getShippingRoutes(
 		@RequestParam UUID departureId,
 		@RequestParam UUID arrivalId,
 		@RequestHeader("X-User-Id") Long userId, @RequestHeader("X-User-Role") UserRoleType role
