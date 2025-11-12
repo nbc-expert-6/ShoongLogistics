e6e2cc3268839bae6bf697c89a7ac962df1a3448
brian110326 <brian110326@gmail.com>
2025-11-12 13:59:30 +0900

    [feat] : User-Service Swagger Cors 설정

---
+++ user-service/src/main/java/com/shoonglogitics/userservice/infrastructure/config/GlobalCorsConfig.java
@@ -0,0 +1,31 @@
+package com.shoonglogitics.userservice.infrastructure.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class GlobalCorsConfig {
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOriginPatterns(List.of(
+			"http://localhost:8000"
+		));
+		config.setExposedHeaders(List.of(
+			"Authorization", "Cache-Control", "Content-Language", "Content-Type",
+			"Expires", "Last-Modified", "Pragma", "X-User-Id", "X-User-Role"
+		));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
+		config.setMaxAge(3600L); // preflight 캐시 시간(초)
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+}
